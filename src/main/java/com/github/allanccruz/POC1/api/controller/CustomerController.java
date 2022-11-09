bc2ca657a36b16540bf665b127d0496edf74018b
Allan Chaves Cruz <allan.9598@gmail.com>
2022-11-09 10:36:31 -0300

    Small change in dependency injection

--- src/main/java/com/github/allanccruz/POC1/api/controller/CustomerController.java
+++ src/main/java/com/github/allanccruz/POC1/api/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.github.allanccruz.POC1.api.controller;
 import com.github.allanccruz.POC1.api.dto.CustomerDto;
 import com.github.allanccruz.POC1.api.entities.Customer;
 import com.github.allanccruz.POC1.api.service.CustomerService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,14 +13,11 @@ import java.util.List;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("api/poc1")
 public class CustomerController {
 
-    final CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    private final CustomerService customerService;
 
     @PostMapping(value = "/customers", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
